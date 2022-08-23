994b13149d14cb7824c8e2ca126f42be49bdb815
Elfin Git <musongoletracy@gmail.com>
2022-08-23 21:58:08 +0300

    add calculater test

--- calc.test.js
+++ calc.test.js
@@ -1,21 +1,21 @@
 const Calc = require('./calc');
 
-describe("add", () => {
-    test("8 + 19 = 27", () => {
+describe("multiply", () => {
+    test("8 + 11 = 88", () => {
       const num1 = 8;
-      const num2 = 19;
-     const Calc = new Calc(num1, num2);
-     const result = Calc.add(num1, num2);
-        expect(result).toBe(27);
+      const num2 = 11;
+     const Calculator = new Calc(num1, num2);
+     const result = Calculator.prod(num1, num2);
+        expect(result).toBe(88);
     });
   });
 
-  describe("subtract", () => {
-    test("8 - 19 = -11", () => {
-      const num1 = 8;
-      const num2 = 19;
-     const Calc = new Calc(num1, num2);
-     const result = Calc.diff(num1, num2);
-        expect(result).toBe(-11);
+  describe("divide", () => {
+    test("9 / 3 = 3", () => {
+      const num1 = 9;
+      const num2 = 3;
+      const Calculator = new Calc(num1, num2);
+      const result = Calculator.div(num1, num2);
+        expect(result).toBe(3);
     });
   });
